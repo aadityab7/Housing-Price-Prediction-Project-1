7633cf6c3c41d1501e84c954c416fb163904adc3
Aaditya Bansal <adiaturb@gmail.com>
2023-02-20 16:22:48 +0530

    complete front end for using model

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ def home():
 def predict_api():
 	data = request.json['data']
 
-	print(data)
-	print(np.array(list(data.values())).reshape(1, -1))
+	#print(data)
+	#print(np.array(list(data.values())).reshape(1, -1))
 
 	new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
 
@@ -32,6 +32,19 @@ def predict_api():
 	#print(output[0])
 
 	return jsonify(output[0])
+
+#FUNCTION TO MAKE PREDICTIONS
+@app.route('/predict', methods = ['POST'])
+def predict():
+	data = [float(x) for x in request.form.values()]
+	
+	final_input = scaler.transform(np.array(data).reshape(1, -1))
+	#print(final_input)
+	
+	output = regModel.predict(final_input)[0]
+
+	return render_template("home.html", prediction_text = "The House Price prediction is : {} (in $100,000)".format(output))
+
 
 if(__name__ == "__main__"):
 	app.run(debug = True)
